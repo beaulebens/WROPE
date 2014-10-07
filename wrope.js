3a975b6c88a6541b71a067b3825d6babb99c2e08
Beau Lebens <beau@dentedreality.com.au>
2014-10-06 22:46:39 -0600

    Initial Commit Contains a working, very simple, client-side app that uses the WP JSON REST API to load 2 batches of posts and render them as a river of posts. Allows clicking through to hash-marked "permalinks" of each. Uses Photon to dynamically optimize images. Bundles a Keynote presentation to introduce this + Backbone.js in the context of WordPress.

---
+++ wrope.js
@@ -0,0 +1,145 @@
+var WROPE = {
+  posts: null,
+
+  fetchPosts: function( callback ) {
+    var posts = new wp.api.collections.Posts();
+    posts.fetch().done( function() {
+      WROPE.posts = posts.clone();
+      if ( posts.hasMore() ) {
+        posts.more().done( function() { // Get a second batch
+          WROPE.posts.add( posts.toJSON() );
+          callback();
+        });
+      } else {
+        callback();
+      }
+    });
+  },
+
+  routerObj: Backbone.Router.extend({
+    root: '/dev/WROPE/',
+
+    routes: {
+      '': 'index',
+      ':post': 'post'
+    },
+
+    index: function() {
+      // Get a collection of posts from WP and render them once returned
+      WROPE.fetchPosts( function() { this.renderRiver(); }.bind( this ) );
+    },
+
+    renderRiver: function() {
+      WROPE.postsRiver = new WROPE.postsView( { collection: WROPE.posts } );
+
+      // Load optimized inline images, and reload them when the page is resized
+      WROPE.optimizeImageSize();
+      $(window).on('resize', _.debounce( WROPE.optimizeImageSize, 500 ) );
+    },
+
+    post: function( post ) {
+      if ( null === WROPE.posts ) {
+        WROPE.fetchPosts( function() { this.renderPost( post ); }.bind( this ) );
+      } else {
+        this.renderPost( post );
+      }
+    },
+
+    renderPost: function( post ) {
+      var thePost = WROPE.posts.get( post );
+      var postView = new WROPE.postView( { model: thePost, tagName: 'div' } );
+      $( '#wrope' ).slideUp( function() {
+        $(this).html( postView.$el ).slideDown();
+        WROPE.optimizeImageSize();
+      });
+      return;
+    }
+  }),
+
+  postsView: Backbone.View.extend({
+    tagName: 'ol',
+
+    className: 'post-river',
+
+    initialize: function( options ) {
+      this.collection = options.collection;
+      this.renderAll();
+    },
+
+    renderAll: function() {
+      this.collection.each( function( post ) {
+        // Filter currently doesn't support ignore_sticky_posts
+        // Let's skip them for this demo app
+        if ( post.get( 'sticky' ) ) {
+          return;
+        }
+        var postView = new WROPE.postView( { model: post } );
+        this.$el.append( postView.$el );
+      }.bind( this ) );
+
+      $( '#wrope' ).html( this.$el );
+    },
+
+    render: function() {
+      // Nothing to render
+      return this;
+    }
+  }),
+
+  postView: Backbone.View.extend({
+    tagName: 'li',
+
+    className: 'post',
+
+    template: _.template( $('#tpl-post').text() ),
+
+    events: {
+      'click a': 'preventDefault',
+      'click h1.post-title a': 'goToPage',
+      'click .featured-image a': 'goToPage'
+    },
+
+    initialize: function( options ) {
+      this.render();
+    },
+
+    preventDefault: function( e ) {
+      e.preventDefault();
+    },
+
+    goToPage: function() {
+      WROPE.router.navigate( '/' + this.model.get( 'ID' ), { trigger: true } );
+      return;
+    },
+
+    render: function() {
+      this.$el.html( this.template( this.model.attributes ) );
+      return this;
+    }
+  }),
+
+  /**
+   * Do some Photon magic. https://developer.wordpress.com/docs/photon/
+   * Technically against the Photon ToS, unless you're using Jetpack on your site!
+   * Takes the Photon-encoded img src URL from 'data-src' attribute, then applies a dynamic crop
+   * via Photon, which matches the screen size (x2 for HiDPI!)
+   */
+  optimizeImageSize: function() {
+    var w = $(document).width();
+    $('.feature').attr( 'src', function() {
+      return $(this).data('src') + '?resize=' + w * 2 + ',400';
+    });
+  },
+
+  /**
+   * Handles all the boot-up for the application, including initial request of
+   * posts, rendering templates, etc.
+   * @return WROPE application object
+   */
+  start: function() {
+    // Set up our router, start monitoring URLs, and get going
+    this.router = new this.routerObj();
+    Backbone.history.start( { root: '/dev/WROPE/' } );
+    return this;
+  }
+};
